fa0cb8c545882cae342ab7ed6cdd1953d35a7777
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-04-25 22:02:37 -0700

    Sidebar basic function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
   });
+
+  $(document).ready(function () {
+
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+
+});
