6334ee19be937dd88f21714af7130fc062e2b8eb
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-04-26 10:07:43 -0700

    Fix page sidebar layout and page structure

--- js/app.js
+++ js/app.js
@@ -4,8 +4,17 @@ $(document).ready(function(){
 
   $(document).ready(function () {
 
-    $('#sidebarCollapse').on('click', function () {
+    $('#side-menu-btn').on('click', function () {
         $('#sidebar').toggleClass('active');
+        $(this).blur();
     });
+
+});
+
+$(document).ready(function () {
+
+  $('#side-close').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
 
 });
