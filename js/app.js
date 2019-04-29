526ba5611e34d070b3f92a4bbb279d67665d9167
Ricardo Carballo <rcarbaleq2@gmail.com>
2019-04-29 10:19:49 -0700

    Refactor page structure to implement BEM

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
-$(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip(); 
+$(document).ready(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(document).ready(function () {
+
+  $('#side-menu-btn').on('click', function () {
+    toggleSidebar();
+    $(this).blur();
   });
-
-  $(document).ready(function () {
-
-    $('#side-menu-btn').on('click', function () {
-        $('#sidebar').toggleClass('active');
-        $(this).blur();
-    });
 
 });
 
 $(document).ready(function () {
 
   $('#side-close').on('click', function () {
-      $('#sidebar').toggleClass('active');
+    toggleSidebar();
   });
 
 });
+
+function toggleSidebar() {
+  $('.sidebar').toggleClass('sidebar--not_active');
+  $('.content').toggleClass('content--active_sidebar');
+}
